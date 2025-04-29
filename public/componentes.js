dbca5ca3b83b2453cf544ae8cde11a6710ccb48f
Diego Lucas Luis <d.lucas.2021@alumnos.urjc.es>
2025-04-29 14:01:27 +0200

    Fase 2

--- public/componentes.js
+++ public/componentes.js
@@ -19,3 +19,38 @@ AFRAME.registerComponent('globo', {
         });
     }
 });
+
+
+AFRAME.registerComponent('movedor', {
+    schema: {
+      velocidad: { type: 'number', default: 1 },
+      intervalo: { type: 'number', default: 2000 }
+    },
+  
+    init: function () {
+      this.direccion = new THREE.Vector3();
+      this._generarNuevaDireccion();
+
+      this.intervaloID = setInterval(() => {
+        this._generarNuevaDireccion();
+      }, this.data.intervalo);
+    },
+  
+    tick: function (time, timeDelta) {
+      const desplazamiento = this.direccion.clone().multiplyScalar(this.data.velocidad * timeDelta / 1000);
+      this.el.object3D.position.add(desplazamiento);
+    },
+  
+    remove: function () {
+      clearInterval(this.intervaloID);
+    },
+  
+    _generarNuevaDireccion: function () {
+      this.direccion.set(
+        Math.random() * 2 - 1,
+        Math.random() * 2 - 1,
+        Math.random() * 2 - 1
+      ).normalize();
+    }
+  });
+  
