f6395e3e10b1ea39d3fff157afb9b6716f950283
Diego Lucas Luis <d.lucas.2021@alumnos.urjc.es>
2025-05-25 19:48:16 +0200

    Fase 7

--- public/componentes2.js
+++ public/componentes2.js
@@ -289,8 +289,8 @@ AFRAME.registerComponent('marcador', {
 
     this.el.setAttribute('geometry', 'primitive: plane; width: 0.6; height: 0.2');
     this.el.setAttribute('material', 'color: white; shader: flat');
-    this.el.setAttribute('position', '-1.2 -0.6 -0.8'); 
-    this.el.setAttribute('scale', '0.5 0.5 0.5'); 
+    this.el.setAttribute('position', '-1 2 -2'); 
+    this.el.setAttribute('scale', '0.8 0.8 0.8'); 
 
     // Título 
     const marcadorTitle = document.createElement('a-text');
